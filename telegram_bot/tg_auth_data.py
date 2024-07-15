7b2cf6840c6de61d4059035cfcc5b341b99c482b
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2024-07-15 09:49:02 +0500

    version 2.0.7+railway+Procfile

--- telegram_bot/tg_auth_data.py
+++ telegram_bot/tg_auth_data.py
@@ -1,4 +1,4 @@
 class BotToken:
     def __init__(self):
-        # self.token = "6334592260:AAG52r7BSR4XW_PALNFt1lZP5hdhLbn8YYc"
-        self.token = "5645212249:AAGJNUmBRZzsF5kJ-jEC0x_WO388M5QZjSw"
+        self.token = "6334592260:AAG52r7BSR4XW_PALNFt1lZP5hdhLbn8YYc"
+        # self.token = "5645212249:AAGJNUmBRZzsF5kJ-jEC0x_WO388M5QZjSw"
