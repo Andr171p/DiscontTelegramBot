35cde3a05dd15bd61124747c777e0418c633f779
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-10-06 12:41:25 +0500

    Discount telegram bot service 1.1.1

--- cache/models.py
+++ cache/models.py
@@ -2,11 +2,11 @@
 
 class RedisUserModel:
     def __init__(
-            self, user_id: bytes | str | int, username: bytes | str, phone: bytes | str
+            self, user_id: int, username: str, phone: str
     ) -> None:
-        self.user_id = user_id.decode('utf-8')
-        self.username = username.decode('utf-8')
-        self.phone = phone.decode('utf-8')
+        self.user_id = user_id
+        self.username = username
+        self.phone = phone
 
     def __repr__(self) -> str:
         return f"RedisUserModel(user_id={self.user_id!r}, username={self.username!r}, phone={self.phone!r}"
